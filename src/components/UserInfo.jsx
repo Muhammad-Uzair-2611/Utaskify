a52085c6f0c8313ccad67d4c848076218a4324cf
Muhammad-Uzair-2611 <muhuzair127@gmail.com>
2025-03-01 00:42:48 +0500

    add Some Files and Working on UserInfo.jsx

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -9,39 +9,33 @@ const UserInfo = () => {
   }, [animate]);
 
   const handleClick = () => {
-    localStorage.setItem("name", name);
+    localStorage.setItem("name", name ? name : "Anonymous");
     localStorage.setItem("isVisible", true);
+    location.reload();
   };
   const handleChange = (e) => {
     setName(e.target.value);
   };
 
   return (
-    <div className="fixed border h-screen w-screen flex justify-center items-center z-50">
-      <div
-        className={`transition-all  ease-in
-           ${
-             animate ? "opacity-100 scale-100" : "opacity-0 scale-0"
-           }  w-2/4 h-60 bg-[#051956] rounded-2xl px-6 py-3 shadow-md shadow-white`}
-      >
-        <h2 className="text-2xl font-bold text-center text-white">
-          Welcome to UTaskify
+    <div className="fixed  h-screen w-screen flex justify-center items-center z-50">
+      <div className="bg-[#1B2430] p-6 rounded-2xl shadow-xl w-80 text-center animate-fadeIn text-white border border-[#526D82]">
+        <h2 className="text-xl font-semibold mb-4 text-[#DDE6ED]">
+          Enter Your Name
         </h2>
-        <div className="flex flex-col gap-y-4 items-center justify-center border ">
-          <label htmlFor="name" className="text-white text-lg ">
-            Please Enter Your Name
-          </label>
-          <input onChange={handleChange} className="" type="text" />
-        </div>
-
-        <a href="/">
-          <button
-            onClick={handleClick}
-            className="bg-[#3550A1] text-white p-2 text-center rounded-lg"
-          >
-            Continue
-          </button>
-        </a>
+        <input
+          type="text"
+          className="w-full px-4 py-2 border border-[#526D82] bg-[#27374D] text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-[#9DB2BF] placeholder-[#9DB2BF]"
+          placeholder="Your Name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          onClick={handleClick}
+          className="mt-4 bg-[#526D82] text-white px-4 py-2 rounded-lg hover:bg-[#9DB2BF] transition"
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
